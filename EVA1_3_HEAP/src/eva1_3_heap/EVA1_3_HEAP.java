94a0f3a57e0d3189e5526434f2647d7eef471649
Jorge Bugarini <64108165+DarkZeyan@users.noreply.github.com>
2022-08-19 09:46:20 -0600

    Add files via upload

---
+++ EVA1_3_HEAP/src/eva1_3_heap/EVA1_3_HEAP.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_3_heap;
+
+/**
+ *
+ * Jorge Eduardo Escobar Bugarini ISC - 21550317 - (9-10)-C EDD 
+ *  
+- */
+public class EVA1_3_HEAP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        int x = 27;
+        Prueba obj = new Prueba();
+        Prueba obj2 = new Prueba();
+        
+        System.out.println(x);
+        System.out.println(obj.valor);
+        System.out.println(obj2);
+        
+    }
+    
+}
+class Prueba{
+    int valor = 100;
+}
