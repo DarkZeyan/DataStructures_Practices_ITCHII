d16daa3c807e23bc1029815b9cc033e9515718c0
Jorge Bugarini <64108165+DarkZeyan@users.noreply.github.com>
2022-09-29 09:54:29 -0600

    Updates have been implemented

--- EVA2_1_LISTA_SIMPLE/src/eva2_1_lista_simple/EVA2_1_LISTA_SIMPLE.java
+++ EVA2_1_LISTA_SIMPLE/src/eva2_1_lista_simple/EVA2_1_LISTA_SIMPLE.java
@@ -7,8 +7,18 @@ public class EVA2_1_LISTA_SIMPLE {
     public static void main(String[] args) {
         List myList =  new List();
         
+     //   int[] arreglo = new int[1000000];
+     
+     //Eficiencia de agregar el millon de nodos = O(N^2)
+//        for(int i=0; i<1000000; i++){
+//            myList.add(i);
+//        }
+
         myList.add(10);
-        myList.add(12);
+        myList.add(50);
+        
         myList.print();
-    }
+        
+        System.out.println("Tamaño de la lista: "+myList.size());
+     }
 }
--- EVA2_1_LISTA_SIMPLE/src/eva2_1_lista_simple/List.java
+++ EVA2_1_LISTA_SIMPLE/src/eva2_1_lista_simple/List.java
@@ -6,15 +6,18 @@ package eva2_1_lista_simple;
  */
 public class List {
     private Node start;
+    private Node last;
+    private int cont=0;
     
     public List(){
         this.start = null;
+        this.last = null;
     }
     
     public void print(){
         Node temp = start;
-        while(temp.getNext() != null){
-            System.out.print(temp.getValue() + " ");
+        while(temp != null){
+            System.out.print(temp.getValue() + " - ");
             temp =  temp.getNext();
         }
     }
@@ -24,20 +27,35 @@ public class List {
     
     public void add(int value){
         Node newNode = new Node(value);
-        
+        cont++;
         if(start ==  null){//No hay nodos en la lista
-             
+             start = newNode;
+             last = newNode;
         }else{ // Existen nodos en la lista.
             /*
             Hay que movernos por la lista
             Usamos un nodo temporal hasta el ultimo nodo de la lista
             */
-            Node temp = start;
-            while(temp.getNext() != null){
-                temp =  temp.getNext();
-            }
-            temp.setNext(newNode);
+//            Node temp = start;
+//            while(temp.getNext() != null){
+//                temp =  temp.getNext();
+//            }
+//            temp.setNext(newNode);
+              last.setNext(newNode);
+              last = newNode;
+              
         }
+        
+    }
+    
+    public int size(){
+        return cont;
+    }
+    
+    public void insertInto(int value, int pos){
+        //Insertar en una posicion no valida
+        //Posiciones negativas o mayores a la cantidad de elementos;
+        
     }
     
     
