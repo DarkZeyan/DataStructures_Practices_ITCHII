3dc7d6cf4492116e40395fcc18787f9f055a88bd
DarkZeyan <esonic0234@hotmail.com>
2022-11-18 09:45:53 -0600

    Finalizado el Selection Sort e inicio de Insertion Sort
    Se implemento la logica de programacion del selection sort y se inicializo con la del insertionSort, asi mismo se soluciono un error de logica que provocaba que el ciclo del selection creara un bucle infinito

--- EVA3_1_ORDENAMIENTOS/src/eva3_1_ordenamientos/EVA3_1_ORDENAMIENTOS.java
+++ EVA3_1_ORDENAMIENTOS/src/eva3_1_ordenamientos/EVA3_1_ORDENAMIENTOS.java
@@ -20,12 +20,14 @@ public class EVA3_1_ORDENAMIENTOS {
         int[] array =  new int[10];
         fill(array);
         //print the array
-        print(array);
+        printArray(array);
         //copy array
         
         int[] arraySel = new int[array.length];
         duplicate(array,arraySel);
-        print(arraySel);
+        printArray(arraySel);
+        selecSort(arraySel);
+        printArray(arraySel);
     }
 
     public static void fill(int[] array) {
@@ -34,7 +36,7 @@ public class EVA3_1_ORDENAMIENTOS {
         }
     }
 
-    public static void print(int[] array) {
+    public static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             if(array[i]<10){
                    System.out.print("[ "+array[i] + "]");
@@ -45,9 +47,43 @@ public class EVA3_1_ORDENAMIENTOS {
         System.out.println("");
     }
     
-    public static void duplicate(int array[], int[] copy){
-        for(int i=0; i < array.length; i++){
-            copy[i] = array[i];
+    public static void duplicate(int array[], int copy[]){
+        for (int i = 0; i < array.length; i++) {
+            copy[i]=array[i];
         }
     }
+    
+    public static void selecSort(int array[]){
+        for(int i=0; i < array.length; i++){
+            int min = i;
+            for(int j=i+1; j<array.length; j++){
+                    if(array[j]<array[min])
+                        min=j;
+            }
+            if(min!=i){
+                int temp = array[i];
+                array[i]=array[min];
+                array[min] = temp;
+            }
+        }
+    }
+
+    public static void insertionSort(int[] array){
+        for(int i=1; i<array.length; i++){
+            int temp = array[i];  // Es lo que se va a ordenar
+            int insertion_point = i; // Se busca en que punto debe de quedarse el temp, esa posicion se almacena en insertion_point
+            for(int prev=i-1; prev>=0; prev--){
+                //Se hace la comparacion
+                if(array[prev]>temp){
+                    //Swap (intercambio parcial)
+                    array[insertion_point]=array[prev];
+                    insertion_point--;
+                }else{
+                    
+                }
+
+            }
+        }
+    }
+
 }
